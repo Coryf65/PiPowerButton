685c3225d1b301a571c5e210bc2f680b22670b66
CoryFabian <Coryf65@gmail.com>
2022-02-11 21:48:22 -0600

    updating

--- cory_install.sh
+++ cory_install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+echo "Starting......"
 
 echo "Downloading button script..."
 curl -O https://raw.githubusercontent.com/Coryf65/PiPowerButton/master/cory_onoff_button.py
 echo "Downloading button service..."
 curl -O https://raw.githubusercontent.com/Coryf65/PiPowerButton/master/cory_button.service
 
-if [ ! -d "/usr/local/bin" ]; then
-    sudo mkdir -p /usr/local/bin
-fi
 
-echo "Setting up..."
+sudo mkdir -p /usr/local/bin
 echo "Making script executable"
 sudo chmod +x cory_onoff_button.py
 sudo mv cory_onoff_button.py /usr/local/bin
