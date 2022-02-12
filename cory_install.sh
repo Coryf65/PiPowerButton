292eb61e725bc5fd1fb18ac2ef3c656e3710d32d
CoryFabian <Coryf65@gmail.com>
2022-02-11 20:36:48 -0600

    fixing service and setup script

--- cory_install.sh
+++ cory_install.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 echo "Downloading button script..."
-curl -O https://raw.githubusercontent.com/
+curl -O https://raw.githubusercontent.com/Coryf65/PiPowerButton/master/cory_onoff_button.py
 echo "Downloading button service..."
-curl -O https://raw.githubusercontent.com/
+curl -O https://raw.githubusercontent.com/Coryf65/PiPowerButton/master/cory_button.service
 
 if [ ! -d "/usr/local/bin" ]; then
     sudo mkdir -p /usr/local/bin
-fi
 
 echo "Setting up..."
 echo "Making script executable"
 sudo chmod +x cory_onoff_button.py
 sudo mv cory_onoff_button.py /usr/local/bin
-sudo mv cory_onoff_button_service.service /etc/systemd/system
+sudo mv cory_button.service /etc/systemd/system
+
 echo "Starting service..."
-sudo systemctl start cory_onoff_button_service.service
+sudo systemctl start cory_button.service
+
 echo "Enabling service..."
-sudo systemctl enable cory_onoff_button_service.service
+sudo systemctl enable cory_button.service
 
 echo "completed setup...."
