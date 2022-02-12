d6749109eb8506450193fb661f1e1822a5e705e1
CoryFabian <Coryf65@gmail.com>
2022-02-11 20:37:19 -0600

    removed uneeded script

--- cory_onoff_button.py
+++ cory_onoff_button.py
@@ -5,9 +5,6 @@ from signal import pause
 def shutdown():
     check_call(['sudo', 'poweroff'])
 
-def powerup():
-    check_call(['sudo', ''])
-
 shutdown_btn = Button(17, hold_time=2)
 shutdown_btn.when_held = shutdown
 
